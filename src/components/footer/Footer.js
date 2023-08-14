f2c10bb588c34e8ac2898f04944321a6f5f771c4
Shiewhun <shiewhunnues@gmail.com>
2023-08-14 14:57:43 +0100

    Update Footer.js

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,11 +7,11 @@ const Footer = () => {
   return (
     <div className='footer'>
         <div className="footer-container">
-            <h4 className="f-lang">Full-stack Web Application Deveopers, Blockchain Developers, Application Intergration & Support, Customer Training.</h4>
-            <h4 className="f-name">	&copy; Copyright, By Grays Devs</h4>
+            <h4 className="f-lang">Full-stack Web Application Developers, Blockchain Interface Developers, Application Integration & Support, Customer Training.</h4>
+            <h4 className="f-name">	&copy; GraysDevs</h4>
             {/* <div className="f-icons">
                 <a href="https://github.com/" target="_blank" rel="noopener noreferrer">Github</a>
-                <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">Tweeter</a>
+                <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">Twitter</a>
                 <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
             </div> */}
             <h4><Link to='/contact'> Lets Talk  </Link></h4>
