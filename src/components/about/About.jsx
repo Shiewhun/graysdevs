fd131e9be63b129ac47f5ede8ce752b372602ab8
Shiewhun <shiewhunnues@gmail.com>
2023-08-14 14:31:43 +0100

    Updated text in About.jsx

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -39,8 +39,9 @@ const About = () => {
           viewport={{once:true, amount:0.7}}
           transition={{staggerChildren:2}}
         >
-          <motion.h1 className="a-in" variants={headAni}>About Us</motion.h1>
-          <motion.h4 className="sum" variants={subAni}>Here you will find more information about us, what we do, and our current skills mostly in terms of programming and technology.</motion.h4>
+          {/* <motion.h1 className="a-in" variants={headAni}>About Us</motion.h1> */}
+          <motion.h1 className="a-in" variants={headAni}>Get to Know Us:</motion.h1>
+          {/* <motion.h4 className="sum" variants={subAni}>Here you will find more information about us, what we do, and our current skills mostly in terms of programming and technology.</motion.h4>  */}
         </motion.div>
         <motion.div className="about-main"
           initial={'offscreen'}
@@ -51,22 +52,22 @@ const About = () => {
         >
           <motion.div className="know" variants={knowAniL}>
             <motion.div className="bar" ></motion.div>
-            <motion.h3>Get to know us!:</motion.h3>
+            // <motion.h3>Get to know us!:</motion.h3>
             <h4>
-              Our teams of experienced engineers is composed of versatile designers and full-stack web developers adept at seamlessly uniting users-centric interfaces with robust backend business logic.
-              Our expertise spans the entire spectrum, from crafting elegant web pages for your customers to navigate, to a backend architecture that implements efficient and reliable software solutions meeting your actual business needs, all together to deliver the perfect web solution for your business.  
+             Our team of experienced engineers is composed of versatile designers and full-stack web developers adept at seamlessly uniting user-centric interfaces with robust backend business logic.
+              Our expertise spans the entire spectrum, from crafting elegant web pages for your customers to navigate, to a backend architecture that implements efficient & reliable software solutions meeting your actual business needs, all together to deliver the perfect web solution for your business.
             </h4>
-            <p>Email Address: <a href="mailto:dammymaccoy1996@gmail.com">dammymaccoy1996@gmail.com</a></p>
-            <p>Phone Number: <a href="tel:+16478923227">+16478923227</a></p>
+            <p>Email Address: <a href="mailto:myremotemailer@gmail.com">hello@graysdevs.com</a></p>
+            <p>Phone Number: <a href="tel:+447729609732">+44 7729 609732</a></p>
             <button className='go-to'>
               <Link to='project' smooth={true} duration={500} delay={1000} offset={-70}>
-                Projects
+                Some of Our Work
               </Link>
             </button>
           </motion.div>
           <motion.div className="my-skills" variants={skiAniR}>
             <div className="bar"></div>
-            <h3>My Skills</h3>
+            <h3>We Work With</h3>
             <div className="skills">
               <div className="skill">HTML</div>
               <div className="skill">CSS</div>
@@ -76,6 +77,7 @@ const About = () => {
               <div className="skill">Styled Components</div>
               {/* <div className="skill">Solidity</div> */}
               <div className="skill">Nextjs</div>
+              <div className="skill">Golang</div>
               {/* <div className="skill">Hardhat</div> */}
             </div>
           </motion.div>
